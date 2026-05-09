ccfd11366402b26a75d38cf0efd59bb28f7e5e61
yyasuhara01-sketch <y.yasuhara01@gmail.com>
2026-05-09 16:32:07 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
+  output: 'static',
 });
